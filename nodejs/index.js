47153a5d1f2eb79ebb115339eb3fac4913520e04
Sai Vedagiri <43285226+SaiVedagiri@users.noreply.github.com>
2021-05-09 18:54:53 -0400

    Add Clarifai API Integration

--- nodejs/index.js
+++ nodejs/index.js
@@ -10,8 +10,13 @@ const NodeRSA = require("node-rsa");
 const jwt = require("jsonwebtoken");
 const AppleAuth = require("apple-auth");
 const dotenv = require("dotenv")
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
 const PORT = process.env.PORT || 80;
 dotenv.config()
+
+const stub = ClarifaiStub.grpc();
+const metadata = new grpc.Metadata();
+metadata.set("authorization", `Key ${process.env.CLARIFAIAPIKEY}`);
 
 const server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
@@ -205,6 +210,39 @@ express()
     let deviceID = info.deviceID;
     await createDevice(deviceID);
     res.sendStatusCode(200);
+  })
+  .post("/analyzeImage", async function (req, res) {
+    let info = req.headers;
+    let imageURL = info.imageurl;
+    let returnVal = {data: "false"};
+    stub.PostModelOutputs(
+      {
+        // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+        model_id: "aaa03c23b3724a16a56b629203edc62c",
+        inputs: [{ data: { image: { url: imageURL } } }]
+      },
+      metadata,
+      (err, response) => {
+        if (err) {
+          console.log("Error: " + err);
+          return;
+        }
+
+        if (response.status.code !== 10000) {
+          console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+          return;
+        }
+
+        console.log("Predicted concepts, with confidence values:")
+        for (const c of response.outputs[0].data.concepts) {
+          console.log(c.name + ": " + c.value);
+          if(c.name == "recycling" || c.name == "plastic" || c.name == "paper"){
+            returnVal.data = "true"
+          }
+        }
+      }
+    );
+    res.send(response);
   })
   .post("/verifyRegistration", async function (req, res) {
     let info = req.headers;
