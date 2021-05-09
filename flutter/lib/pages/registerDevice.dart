6831c5b03579dfc5aa618628d56f3cf097d37c56
Sai Vedagiri <43285226+SaiVedagiri@users.noreply.github.com>
2021-05-09 18:22:52 -0400

    Add device registration

--- flutter/lib/pages/registerDevice.dart
+++ flutter/lib/pages/registerDevice.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:barcode_scan2/barcode_scan2.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+var prefs;
 
 class RegisterDevicePage extends StatefulWidget {
   RegisterDevicePage({Key? key}) : super(key: key);
@@ -9,6 +13,16 @@ class RegisterDevicePage extends StatefulWidget {
 }
 
 class _RegisterDevicePageState extends State<RegisterDevicePage> {
+  @override
+  initState() {
+    super.initState();
+    initStateFunction();
+  }
+
+  initStateFunction() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
   Future<dynamic> createAlertDialog(
       BuildContext context, String title, String body) {
     return showDialog(
@@ -62,7 +76,51 @@ class _RegisterDevicePageState extends State<RegisterDevicePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+              child: Text(
+                "Scan the QR code attached to your EcoSort device.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: FloatingActionButton.extended(
+                  icon: Icon(Icons.camera),
+                  label: Text("Scan QR"),
+                  onPressed: () async {
+                    try {
+                      ScanResult qrResult = await BarcodeScanner.scan();
+                      var result = qrResult.rawContent;
+                      prefs.setString('deviceID', result);
+                    } on PlatformException catch (ex) {
+                      if (ex.code == BarcodeScanner.cameraAccessDenied) {
+                        setState(() {
+                          createAlertDialog(context, "Scan QR",
+                              "Please enable camera permissions for Grove App.");
+                        });
+                      } else {
+                        setState(() {
+                          createAlertDialog(context, "Scan QR",
+                              "Unknown Error Occurred: $ex");
+                        });
+                      }
+                    } on FormatException {
+                      setState(() {
+                        createAlertDialog(
+                            context, "Scan QR", "No QR Code was recognized.");
+                      });
+                    } catch (ex) {
+                      setState(() {
+                        createAlertDialog(
+                            context, "Scan QR", "Unknown Error Occurred: $ex");
+                      });
+                    }
+                  }),
+            ),
+          ],
         ),
       ),
     );
