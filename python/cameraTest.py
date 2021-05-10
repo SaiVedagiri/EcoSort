d98044322069ca1c7c397477697e94e4b1d2bac1
Albert <albertrzou@gmail.com>
2021-05-10 00:34:35 -0400

    working google computervision api with camera

--- python/cameraTest.py
+++ python/cameraTest.py
@@ -1,6 +1,22 @@
 import os
-
+from imgurpython import ImgurClient
+import requests
 
 if __name__ == "__main__":
+    client_id = '20c527d1ac24c2f'
+    client_secret = '4201a242a8a0b413fd6d508101c927b9ca9c9b9f'
+
+    client = ImgurClient(client_id, client_secret)
+
     stream = os.popen('fswebcam --no-banner ../images/image.jpg')
     output = stream.read()
+
+    myImage = client.upload_from_path('../images/image.jpg', config=None, anon=True)
+
+    print(myImage['link'])
+
+    res1 = requests.post("https://ecosort.saivedagiri.com/analyzeImageClarifai", headers={"imageurl": myImage['link']})
+    res2 = requests.post("https://ecosort.saivedagiri.com/analyzeImageGoogle", headers={"imageurl": myImage['link']})
+
+    print(res1.text)
+    print(res2.text)
